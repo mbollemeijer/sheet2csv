cc9ebc44923898dfcca07a19846933c7ac1ca3f5
Maikel Bollemeijer <maikel.bollemeijer@acorel.nl>
2021-11-25 08:48:49 +0100

    Fully functional xlsx -> csv converter. Not all wanted features implemented yet

---
+++ run.sh
@@ -0,0 +1,1 @@
+cargo run -- --source="$PWD/examples/test.xlsx" --out="$PWD/examples" --config="$PWD/examples/config.json"
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,29 @@
-use std::{fs::{self, File}, path::PathBuf};
+use std::{env, fs::{self, File}, path::PathBuf};
 
 use calamine::{DataType, Reader, Xlsx, open_workbook};
 use serde::{Deserialize, Serialize};
 
 use std::io::{BufWriter, Write};
 
-// use std::env;
-
-// use calamine::{Reader, Xlsx, open_workbook};
-
-// #[derive(Serialize, Deserialize)]
-// pub struct ConvertSettings {
-//     sheet_settings: Vec<SheetSettings>
-// }
-
 #[derive(Serialize, Deserialize, Debug)]
 #[serde( rename_all = "camelCase")]
 struct SheetSettings {
     sheet_name: String,
-    output_file_name: String
+    output_file_name: String,
+    start_row_index: i8,
 }
 
 fn main() {
 
     // For debug purposes
-    // let args: Vec<String> = env::args().collect();
-    // println!("all arguments in a list are {:?}", &args);
-    //
-    // // Grabbing the arguments
-    // let source = get_program_argument(&args, "--source");
-    // let output_path = get_program_argument(&args, "--out");
-    //
-    // println!("");
-    // println!("source is file: {}", source);
-    // println!("output path:    {}", output_path);
+    let args: Vec<String> = env::args().collect();
+    println!("all arguments in a list are {:?}", &args);
     
-    let output_path = "/home/mbollemeijer/dev/playground/rust-test/";
-    let settings = get_settings("/home/mbollemeijer/dev/playground/rust-test/config.json");
-    let source_file = "/home/mbollemeijer/dev/playground/rust-test/test.xlsx";
-    
+    // Grabbing the arguments
+    let source_file = get_program_argument(&args, "--source");
+    let output_path = get_program_argument(&args, "--out");
+    let config_path = get_program_argument(&args, "--config");
+    let settings = get_settings(&config_path);
 
     let _result = convert_workbook_to_csv(&source_file, &output_path, &settings);
 }
@@ -50,29 +35,28 @@ fn convert_workbook_to_csv(source_file: &str, output_path: &str, settings: &Vec<
         let mut workbook: Xlsx<_> = open_workbook(source_file).expect("Cannot open file");
         // Read whole worksheet data and provide some statistics
         if let Some(Ok(sheet)) = workbook.worksheet_range(&setting.sheet_name) {
-
             let sce = PathBuf::from(output_path.to_owned() + "/" + &setting.output_file_name);
             let dest = sce.with_extension("csv");
             let mut dest = BufWriter::new(File::create(dest).unwrap());
 
-            let n = sheet.get_size().1 - 1;
-            for row in sheet.rows() {
-                for (i, c) in row.iter().enumerate() {
-                    match *c {
-                        DataType::Empty => write!(dest, "{}", ""),
-                        DataType::String(ref s) => write!(dest, "{}", s),
-                        DataType::Int(ref i) => write!(dest, "{}", i),
-                        DataType::Float(ref f) => write!(dest, "{}", f),
-                        DataType::Bool(ref b) => write!(dest, "{}", b),
-                        DataType::DateTime(ref dt) => write!(dest, "{}",dt),
-                        DataType::Error(ref e) => write!(dest, "{}", e),
-                    }?;
-                    if i != n {
-                        write!(dest, ";")?;
+            
+            for (index, row) in sheet.rows().enumerate() {
+                if index >= setting.start_row_index as usize {
+                    for (_i, c) in row.iter().enumerate() {
+
+                        match *c {
+                            DataType::Empty => write!(dest, ";"),
+                            DataType::String(ref s) => write!(dest, "\"{}\";", s),
+                            DataType::Int(ref i) => write!(dest, "{};", i),
+                            DataType::Float(ref f) => write!(dest, "{};", f),
+                            DataType::Bool(ref b) => write!(dest, "{};", b),
+                            DataType::DateTime(ref dt) => write!(dest, "{};",dt),
+                            DataType::Error(ref e) => write!(dest, "{};", e),
+                        }?;
                     }
+                    write!(dest, "\n")?;
                 }
             }
-            write!(dest, "\r\n")?;
         }
     }
     Ok(())
@@ -85,7 +69,7 @@ fn get_settings(path: &str) -> Vec<SheetSettings> {
     return vec_of_sheet_settings;
 }
 
-fn _get_program_argument(args: &Vec<String>, program_argument: &str) -> String {
+fn get_program_argument(args: &Vec<String>, program_argument: &str) -> String {
 
     let value = args.iter()
         .filter(|e| e.contains("--"))
