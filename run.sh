b7e7e0e2bc25f94e851d557f25d9ba92ebaefbb3
Maikel Bollemeijer <maikel.bollemeijer@acorel.nl>
2021-12-04 13:51:09 +0100

    - Added String subsitute features. - Added wrapStrings boolean to parent config - Added wrapStringChar to parent config - Added wrapStrings boolean to sheet config - Added wrapStringChar to sheet config - Added parent config format in the settings.json     - sourceFile and outputPath can now be specified by settings     - source and outputPath are overriden when pressent as commandline       arguments.

--- run.sh
+++
@@ -1,1 +0,0 @@
-cargo run -- --source="$PWD/examples/test.xlsx" --out="$PWD/examples" --config="$PWD/examples/config.json"
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::{self, File}, path::PathBuf};
+use std::{env, fs::{self, File}, path::PathBuf, time::Instant};
 
 use calamine::{DataType, Reader, Xlsx, open_workbook};
 use serde::{Deserialize, Serialize};
@@ -7,12 +7,33 @@ use std::io::{BufWriter, Write};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde( rename_all = "camelCase")]
+struct Settings {
+    source_file: String,
+    output_path: String,
+    sheet_settings: Vec<SheetSettings>,
+    substitutions: Option<Vec<Substitution>>,
+    wrap_strings: Option<bool>,
+    wrap_string_char: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde( rename_all = "camelCase")]
 struct SheetSettings {
     sheet_name: String,
     output_file_name: String,
     start_row_index: Option<i32>,
     end_row_index: Option<i32>,
-    separator: Option<String>
+    separator: Option<String>,
+    substitutions: Option<Vec<Substitution>>,
+    wrap_strings: Option<bool>,
+    wrap_string_char: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde( rename_all = "camelCase")]
+struct Substitution {
+    match_on: String,
+    replace_with: String
 }
 
 impl SheetSettings {
@@ -47,108 +68,127 @@ fn main() {
 
     // For debug purposes
     let args: Vec<String> = env::args().collect();
-    
     // Grabbing the arguments
     let source_file = get_program_argument(&args, "--source");
     let output_path = get_program_argument(&args, "--out");
     let config_path = get_program_argument(&args, "--config");
-    let settings = get_settings(&config_path);
+    let settings = get_settings(config_path, source_file, output_path);
 
-    let _result = convert_workbook_to_csv(&source_file, &output_path, &settings);
+    let result = convert_workbook_to_csv(settings);
+
+    println!("{:?}", result);
 }
 
-fn convert_workbook_to_csv(source_file: &str, output_path: &str, settings: &Vec<SheetSettings>) -> std::io::Result<()> {
+fn convert_workbook_to_csv(settings: Settings) -> std::io::Result<()> {
 
+    let start_on_workbook_time = Instant::now();
     // Read whole worksheet data and provide some statistics
-    let mut workbook: Xlsx<_> = open_workbook(source_file).expect("Cannot open file");
-    for setting in settings {
-    
-        if let Some(Ok(sheet)) = workbook.worksheet_range(&setting.sheet_name) {
+    println!("opening workbook: {}", &settings.source_file);
+    let mut workbook: Xlsx<_> = open_workbook(&settings.source_file).expect(format!("Could not open file @ {} ", &settings.source_file).as_str());
 
-            let sce = PathBuf::from(output_path.to_owned() + "/" + &setting.output_file_name);
+    for sheet_settings in settings.sheet_settings {
+        let sheet_time = Instant::now();
+        if let Some(Ok(sheet)) = workbook.worksheet_range(&sheet_settings.sheet_name) {
+            let sce = PathBuf::from(settings.output_path.to_owned() + "/" + &sheet_settings.output_file_name);
             let dest = sce.with_extension("csv");
             let mut dest = BufWriter::new(File::create(dest).unwrap());
             
+
+            let wrap = match sheet_settings.wrap_strings {
+                Some(value) => value,
+                None => match settings.wrap_strings {
+                    None => true,
+                    Some(value) => value
+                }
+            };
+
+            let wrap_char = match sheet_settings.wrap_string_char.as_ref() {
+                None =>  match settings.wrap_string_char.as_ref() {
+                    None => String::from("\""),
+                    Some(value) => value.to_string()
+                }
+                Some(v) => v.to_string()
+            };
+
             for (index, row) in sheet.rows().enumerate() {
-                if index >= setting.start_index_or_default() as usize 
-                    && index <= setting.end_index_or_default() as usize {
+                if index >= sheet_settings.start_index_or_default() as usize 
+                    && index <= sheet_settings.end_index_or_default() as usize {
                     for (_i, c) in row.iter().enumerate() {
                         match *c {
-                            DataType::Empty => write!(dest, "{}", setting.separator_or_default()),
-                            DataType::String(ref s) => write!(dest, "{}{}", escape_if_needed(s), setting.separator_or_default()),
-                            DataType::Int(ref i) => write!(dest, "{}{}", i, setting.separator_or_default()),
-                            DataType::Float(ref f) => write!(dest, "{}{}", f, setting.separator_or_default()),
-                            DataType::Bool(ref b) => write!(dest, "{}{}", b, setting.separator_or_default()),
-                            DataType::DateTime(ref dt) => write!(dest, "{}{}",dt, setting.separator_or_default()),
-                            DataType::Error(ref e) => write!(dest, "{}{}", e, setting.separator_or_default()),
+                            DataType::Empty => write!(dest, "{}", sheet_settings.separator_or_default()),
+                            DataType::String(ref string_value) 
+                                => write!(dest, "{}{s}", 
+                                          process_cell(&settings.substitutions, &wrap, &wrap_char, string_value), s = sheet_settings.separator_or_default()), 
+                            DataType::Int(ref i) => write!(dest, "{}{}", i, sheet_settings.separator_or_default()),
+                            DataType::Float(ref f) => write!(dest, "{}{}", f, sheet_settings.separator_or_default()),
+                            DataType::Bool(ref b) => write!(dest, "{}{}", b, sheet_settings.separator_or_default()),
+                            DataType::DateTime(ref dt) => write!(dest, "{}{}",dt, sheet_settings.separator_or_default()),
+                            DataType::Error(ref e) => write!(dest, "{}{}", e, sheet_settings.separator_or_default()),
                         }?;
                     }
                     write!(dest, "\r\n")?;
                 } 
             }
+
+            println!("{} {}s", sheet_settings.sheet_name, sheet_time.elapsed().as_secs());
         }
     }
+    println!("Total: {}s", start_on_workbook_time.elapsed().as_secs());
     Ok(())
 }
 
-fn escape_if_needed(cell_value: &str) -> String {
-    
-    let mut escaped_value = String::from(cell_value);
+fn process_cell(subs: &Option<Vec<Substitution>>, wrap: &bool, wrap_char: &String, cell_value: &str) -> String {
 
-
-    if escaped_value.contains("\n") {
-        escaped_value = escaped_value.replace("\n", "");
+    let subbed_value = sub_values(&subs, cell_value);
+    match wrap {
+        true => format!("{wrap_char}{value}{wrap_char}", value = subbed_value, wrap_char = wrap_char),
+        false => subbed_value
     }
-
-    if escaped_value.contains("\r"){
-        escaped_value = escaped_value.replace("\r", "");
-    }
-
-    if escaped_value.contains("\r\n") {
-        escaped_value = escaped_value.replace("\r\n", "");
-    }
-
-    if escaped_value.contains("\n\r") {
-        escaped_value = escaped_value.replace("\n\r", "");
-    }
-
-    if escaped_value.contains("\"") {
-
-        // TODO 
-        // This should also be set through conversion settings.
-        // Something like: replace: ("something that should match", "")
-        escaped_value = escaped_value.replace("\"", "\"\"");
-    }
-
-    
-    // TODO
-    // This should Dynamic by the separator you have set.
-    
-   // escaped_value = format!("\"{}\"", escaped_value);
-   // escaped_value
-    format!("\"{}\"", escaped_value)
 }
 
-fn get_settings(path: &str) -> Vec<SheetSettings> {
+fn sub_values(subs: &Option<Vec<Substitution>>, cell_value: &str) -> String {
 
-    let settings = fs::read_to_string(path).unwrap();
-    let vec_of_sheet_settings: Vec<SheetSettings> = serde_json::from_str(&settings).unwrap();
-    return vec_of_sheet_settings;
+    let mut subbed_value = String::from(cell_value);
+    if subs.is_some() {
+        for sub in subs.as_ref().unwrap() {
+            if subbed_value.contains(&sub.match_on) {
+                subbed_value = subbed_value.replace(&sub.match_on, &sub.replace_with);
+            }
+        }
+    }
+
+   subbed_value 
 }
 
-fn get_program_argument(args: &Vec<String>, program_argument: &str) -> String {
+
+fn get_settings(config_path: Option<String>, source_file: Option<String>, output_path: Option<String>) -> Settings {
+
+    let settings_file = fs::read_to_string(config_path.unwrap()).expect("Could not read file ");
+    let mut settings: Settings = serde_json::from_str(&settings_file).expect("Could not parse settings file");
+    if source_file.is_some() {
+        settings.source_file = source_file.unwrap();
+    }
+
+    if output_path.is_some() {
+        settings.output_path = output_path.unwrap();
+    }
+
+    return settings;
+}
+
+fn get_program_argument(args: &Vec<String>, program_argument: &str) -> Option<String> {
 
     let value = args.iter()
         .filter(|e| e.contains("--"))
         .find(|e| e.contains(program_argument));
 
     match value {
-        None => format!("argument {} is mandatory but was not found", program_argument),
+        None => None,
         Some(value) => {
             let val = value.split("=").nth(1);
             match val {
-                None => format!("found argument {} but no value was given", program_argument),
-                Some(val) => val.to_string()
+                None => None,
+                Some(val) => Some(val.to_string())
             }
         }
     }
